760ff8ce490a8a68e03115da10e307bf56137607
edumoreira7 <161092739+edumoreira7@users.noreply.github.com>
2024-04-13 00:23:10 +0000

    dividindo em funcoes e criando o menu

--- main.py
+++ main.py
@@ -1,42 +1,113 @@
-from math import *
+from math import cos, sin, radians, sqrt
 
-y0 = float(input("Posição inicial: "))
-h_inicial = float(input("Altura a partir do solo: "))
-v0 = float(input("Velocidade inicial: "))
-angulo = float(input("Ângulo de lançamento: "))
-td = float(input("Instante: "))
-
+def menu():
+  while True:
+    print("1 - Velocidades nas direções î e j")
+    print("2 - Altura máxima")
+    print("3 - Tempo no ar")
+    print("4 - Alcance maximo horizontal")
+    print("5 - Posição x e y no instante")
+    print("6 - Velocidade x e y no instante e módulo")
+    print("7 - Velocidades ao atingir o chão")
+    print("0- Sair\n")
+    opcao = int(input("Digite a opção desejada: "))
+    print()
+    if opcao == 1:
+      velocidades()
+      print()
+      print(f"Velocidade î: {v0x} m/s")
+      print(f"Velocidade j: {v0y} m/s\n")
+    elif opcao == 2:
+      altura_max()
+      print()
+      print(f"Altura máxima: {h} m\n")
+    elif opcao == 3:
+      tempo_subida()
+      print()
+      print(f"Tempo no ar: {t_ar} s\n")
+    elif opcao == 4:
+      alcance_horiontal()
+      print()
+      print(f"Alcance horizontal: {x_max} m\n")
+    elif opcao == 5:
+      posicaoXYi()
+      print()
+      print(f"Posição x no instante: {x} m")
+      print(f"Posição y no instante: {y} m\n")
+    elif opcao == 6:
+      velocidadesXYi()
+      print()
+      print(f"Velocidade x no instante: {vx} m/s")
+      print(f"Velocidade y no instante: {vy} m/s")
+      print(f"Módulo da velocidade: {modulo} m/s\n")
+    elif opcao == 7:
+      velocidadeF()
+      print()
+      print(f"Velocidade x final: {vx_final} m/s")
+      print(f"Velocidade y final: {vy_final} m/s")
+      print(f"Módulo da velocidade final: {modulo_final} m/s\n")
+    elif opcao == 0:
+      print("Saindo...")
+      break
+            
 g = 9.8
 
+def velocidades():
+  global v0x, v0y
 # Velocidades iniciais nas direções î e j
-v0x_rad = v0 * cos(radians(angulo))
-v0y_rad = v0 * sin(radians(angulo))
+  v0 = float(input("Velocidade inicial: "))
+  angulo = float(input("Ângulo de lançamento: "))
+  v0x_rad = v0 * cos(radians(angulo))
+  v0y_rad = v0 * sin(radians(angulo))
 
-v0x = v0 * v0x_rad
-v0y = v0 * v0y_rad
+  v0x = v0 * v0x_rad
+  v0y = v0 * v0y_rad
 
-# Altura máxima
-h = (v0y ** 2 + 2 * g * h_inicial) / (2 * g)
+def altura_max():
+  global h 
+  # Altura máxima
+  velocidades()
+  h_inicial = float(input("Altura a partir do solo: "))
+  h = (v0y ** 2 + 2 * g * h_inicial) / (2 * g)
 
-# Tempo no ar
-t_sub = v0y / g
-t_queda = sqrt(2 * h / g)
-t_ar = t_sub + t_queda
+def tempo_subida():
+  global t_ar
+  # Tempo no ar
+  altura_max()
+  t_sub = v0y / g
+  t_queda = sqrt(2 * h / g)
+  t_ar = t_sub + t_queda
 
-# Alcance máximo horizontal
-x_max = v0x * t_ar
+def alcance_horiontal():
+  global x_max
+  # Alcance máximo horizontal
+  tempo_subida()
+  x_max = v0x * t_ar
 
-# Posições x e y em determinado instante
-x = v0x * td
-y = h_inicial + v0y * td - (g * td**2) / 2
+def posicaoXYi():
+  global x, y, td, h_inicial
+  # Posições x e y em determinado instante
+  alcance_horiontal()
+  td = float(input("Instante: "))
+  h_inicial = float(input("Altura a partir do solo: "))
+  x = v0x * td
+  y = h_inicial + v0y * td - (g * td**2) / 2
 
-# Velocidades x e y e módulo em determinado instante
-vx = x / td
-vy = 2 * (y - h_inicial) / td - (v0y / 2)
-modulo = sqrt(vx**2 + vy**2)
+def velocidadesXYi():
+  global modulo, vx, vy
+  # Velocidades x e y e módulo em determinado instante
+  posicaoXYi()
+  vx = x / td
+  vy = 2 * (y - h_inicial) / td - (v0y / 2)
+  modulo = sqrt(vx**2 + vy**2)
 
-# Velocidades ao atingir o chão
-vx_final = v0x
-y_final = h_inicial + v0y * t_ar - (g * t_ar**2) / 2
-vy_final = 2 * (y_final - h_inicial) / t_ar - (v0y / 2)
-modulo_final = sqrt(vx_final**2 + vy_final**2)
+def velocidadeF():
+  global vx_final, vy_final, modulo_final
+  # Velocidades ao atingir o chão
+  velocidadesXYi()
+  vx_final = v0x
+  y_final = h_inicial + v0y * t_ar - (g * t_ar**2) / 2
+  vy_final = 2 * (y_final - h_inicial) / t_ar - (v0y / 2)
+  modulo_final = sqrt(vx_final**2 + vy_final**2)
+
+menu()
